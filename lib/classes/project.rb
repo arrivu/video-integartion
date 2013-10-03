3fd002c6f0c79843453675c6a537d2b2bd209b4e
natarajan1 <natarajan@arrivusystems.com>
2013-10-03 17:37:45 +0530

    Initial Commit

--- lib/classes/project.rb
+++ lib/classes/project.rb
@@ -28,5 +28,28 @@ module WistiaREST
       JSON.parse(RestClient.delete url)
     end
 
+    # if response["activation"].nil? 
+    #   response #sukumar@gmail.com is already shared on this project
+    # else
+    #   response["activation"] #http://sukumar-inapp.wistia.com/activate/a1c8d52f04
+    #   res.headers[:Location]
+    # end
+    def share_project(project_id,email,requirePassword=0)
+      begin
+        url = "#{@@api_root_url}/projects/#{project_id}/sharings.json?api_password=#{@@api_password}"
+        data = {"with" => email,"requirePassword" => requirePassword }
+        response=RestClient.post url,data
+      rescue => e
+        e.response
+      end
+    end
+
+    #https://api.wistia.com/v1/projects/<project-id>/sharings/<sharing-id>.json
+    #url=https://api.wistia.com/v1/projects/476181/sharings/1001407
+    def delete_share_project(url)
+      url ="#{url}?api_password=#{@@api_password}"
+      response=RestClient.delete url
+    end
+
   end
 end
